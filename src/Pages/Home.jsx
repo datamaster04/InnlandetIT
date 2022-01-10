7c1743e8bae29ad7d2f37012c4571336f6113dc6
datamaster04 <datamaster04@gmail.com>
2022-01-10 13:40:07 +0100

    app

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <section>
+        <div class="row_home">
+            <div class="overskrift_home">
+            Stabben
+            </div>
+
+            <div class="col-lg-4 ansatt-item boxnr1" style="padding: 25px;">
+            <div class="ansatt-item-image imgborder100">
+                <div id="rune_box">
+                <div class="img_rune"><img src="/images/rune.jpg" />
+                </div>
+                <div class="ansatt-item-box">
+                <div class="ansatt-item-name">
+                    Rolf Karlsson
+                </div>
+                <div class="ansatt-item-title">
+                    Daglig leder
+                </div>
+                <div class="ansatt-item-summary">
+                    <p class="rolf_tekst">
+                    Lang og bred erfaring fra mange områder 
+                    innen IKT og IKT ledelse. Kvalifikasjoner 
+                    innen IT design, arkitektur, prosjektledelse, 
+                    agile prosesser, produkt- og systemutvikling. 
+                    Tidligere vært utvikler/programmerer og DBA. 
+                    Spesiell erfaring med integrasjon av interne 
+                    og eksterne datakilder som gjøres tilgjengelig 
+                    for et stort antall brukere
+                    </p>
+                </div>
+                </div>
+                
+                </div>
+            </div>
+            </div>
+
+            <div class="col-lg-4 ansatt-item boxnr1" style="padding: 25px;">
+            <div class="ansatt-item-image imgborder100">
+                <div id="sem-box">
+                <div class="img_sem"><img src="/images/rune.jpg" />
+                </div>
+                <div class="ansatt-item-box">
+                <div class="ansatt-item-name">
+                    Rolf Karlsson
+                </div>
+                <div class="ansatt-item-title">
+                    Daglig leder
+                </div>
+                <div class="ansatt-item-summary">
+                    <p class="sem_tekst">
+                    Lang og bred erfaring fra mange områder 
+                    innen IKT og IKT ledelse. Kvalifikasjoner 
+                    innen IT design, arkitektur, prosjektledelse, 
+                    agile prosesser, produkt- og systemutvikling. 
+                    Tidligere vært utvikler/programmerer og DBA. 
+                    Spesiell erfaring med integrasjon av interne 
+                    og eksterne datakilder som gjøres tilgjengelig 
+                    for et stort antall brukere
+                    </p>
+                </div>
+                </div>
+                </div>
+
+            </div>
+            </div>
+        </div>
+        </section>
+    )
+}
